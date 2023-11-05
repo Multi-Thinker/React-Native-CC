05d5068f8d10ec384dc86c5bc347c074c11675bb
Talha Habib <multi.thinker.1@outlook.com>
2023-11-05 23:12:56 +0500

    add pay to click on cards layout

--- src/components/shared/CreditCardInfo.tsx
+++ src/components/shared/CreditCardInfo.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useState} from 'react';
+import {Button, Text, TextInput, TouchableOpacity, View} from 'react-native';
 import {CreditCardHeading, CreditCardValueHeading} from './CreditCardText';
 import {t} from 'react-native-tailwindcss';
 import CreditCardNumber from './CreditCardNumber';
 import {Icon, ImageTypes} from './Icons';
+import * as Yup from 'yup';
 
 const CreditCard = ({
   name,
@@ -16,6 +17,25 @@ const CreditCard = ({
   type: ImageTypes;
   lastFourDigits: string;
 }) => {
+  const [shouldPay, setShouldPay] = useState(false);
+  const [amount, setAmount] = useState('');
+  const [error, setError] = useState(null);
+  const schema = Yup.number()
+    .required('Please enter an amount.')
+    .typeError('Please enter a valid number.')
+    .min(5, 'Minimum amount is 5.')
+    .max(100, 'Maximum amount is 100.');
+
+  const performPayment = async () => {};
+  const handlePay = () => {
+    schema
+      .validate(amount)
+      .then(async () => {
+        await performPayment();
+        setShouldPay(false);
+      })
+      .catch(error => setError(error.message));
+  };
   return (
     <View
       style={[
@@ -34,27 +54,67 @@ const CreditCard = ({
           shadowColor: '#000',
         },
       ]}>
-      <View style={[t.wFull]}>
-        <Icon image={type} />
-      </View>
-      <CreditCardNumber number={lastFourDigits} />
-      <View
-        style={[
-          t.wFull,
-          {height: 44},
-          t.flex1,
-          t.itemsCenter,
-          t.justifyBetween,
-        ]}>
-        <View style={[t.wFull, {height: 17}]}>
-          <CreditCardHeading
-            headings={[{text: 'Name on Card'}, {text: 'Expires'}]}
-          />
-        </View>
-        <View style={[t.wFull, {height: 30}]}>
-          <CreditCardValueHeading values={[{text: name}, {text: expires}]} />
-        </View>
-      </View>
+      <TouchableOpacity
+        activeOpacity={1}
+        onPress={() => {
+          setShouldPay(true);
+          setError('');
+          setAmount(null);
+        }}
+        style={[t.wFull]}>
+        {!shouldPay ? (
+          <>
+            <View style={[t.wFull]}>
+              <Icon image={type} />
+            </View>
+            <CreditCardNumber number={lastFourDigits} />
+            <View
+              style={[
+                t.wFull,
+                {height: 44},
+                t.flex1,
+                t.itemsCenter,
+                t.justifyBetween,
+              ]}>
+              <View style={[t.wFull, {height: 17}]}>
+                <CreditCardHeading
+                  headings={[{text: 'Name on Card'}, {text: 'Expires'}]}
+                />
+              </View>
+              <View style={[t.wFull, {height: 30}]}>
+                <CreditCardValueHeading
+                  values={[{text: name}, {text: expires}]}
+                />
+              </View>
+            </View>
+          </>
+        ) : (
+          <>
+            <View style={[t.flex]}>
+              <View
+                style={[
+                  t.flexRow,
+                  t.itemsCenter,
+                  t.justifyCenter,
+                  t.selfCenter,
+                  t.wFull,
+                  t.mT10,
+                  {height: 50},
+                ]}>
+                <TextInput
+                  value={amount}
+                  onChangeText={setAmount}
+                  keyboardType="numeric"
+                  placeholder="Enter amount"
+                  style={[t.borderB2, t.mR2]}
+                />
+                <Button title="Pay" onPress={handlePay} />
+              </View>
+              {error && <Text style={{color: 'red'}}>{error}</Text>}
+            </View>
+          </>
+        )}
+      </TouchableOpacity>
     </View>
   );
 };
